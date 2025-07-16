be33b3adf11e43f5a50152ff301811d4fe55b1d7
Bastian Eisenmann <bastian.eisenmann@outlook.com>
2025-07-16 16:02:50 +0200

    Plugin nvim-treesitter konfiguriert

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,15 @@
-return {{
+return {
+  {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   branch = 'main',
   build = ':TSUpdate',
-  ensure_installed = { "yaml", "lua", "bash", "json", "java"},
-  indent = { enable = true },
-  highlight = {
+  opts = {
+    ensure_installed = { "yaml", "lua", "bash", "json", "java"},
+    indent = { enable = true },
+    highlight = {
     enable = true
-  }
-},}
+    }
+}
+  },
+}
